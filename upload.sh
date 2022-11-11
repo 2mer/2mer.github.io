8e094a1cde40b311a065c8eecef189a300bc848f
Tomer Atar <kenyweb7@gmail.com>
2022-11-11 02:42:49 +0200

    fixed upload

--- upload.sh
+++ upload.sh
@@ -18,6 +18,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:2mer/2mer.github.io.git main
+git push -f git@github.com:2mer/2mer.github.io.git gh-pages
 
 cd -
